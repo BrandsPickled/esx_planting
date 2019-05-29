4065e868700668c1f16ee9cb0e3d6e8c1d450f7d
BrandsPickled <brandonvjcooley@gmail.com>
2019-05-29 11:38:45 +0100

    Add files via upload

---
+++ locales/en.lua
@@ -0,0 +1,9 @@
+Locales["en"] = {
+  ["cancel"] = "Operation canceled.",
+  ["vehicle_deny"] = "Unfortunately, you can not be in the vehicle.",
+  ["wait_end"] = "Unfortunately, you must wait until the previous action ends.",
+  ["wrong_place"] = "Unfortunately, you can not do it here.",
+  ["doing_action"] = "You are doing an operation. It takes about 40 seconds.",
+  ["how_cancel"] = "To cancel the operation press <button type='button'>X</button>",
+  ["wait_ten_sec"] = 'Wait at least 10 seconds before restarting this operation.',
+}
